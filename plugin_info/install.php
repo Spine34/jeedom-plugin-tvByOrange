ec32ce7ea8afde7137c631787dd65a952c21213b
Spine34 <raphael.lhomme@free.fr>
2024-10-28 14:12:25 +0100

    update 3 files, delete 2 files and create 1 file

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -18,13 +18,10 @@
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
 // Fonction exécutée automatiquement après l'installation du plugin
-function template_install() {
-}
+function tvByOrange_install() {}
 
 // Fonction exécutée automatiquement après la mise à jour du plugin
-function template_update() {
-}
+function tvByOrange_update() {}
 
 // Fonction exécutée automatiquement après la suppression du plugin
-function template_remove() {
-}
+function tvByOrange_remove() {}
